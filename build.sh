7402fa135636002a6d764dce5088554b68288cb1
Álan Crístoffer <acristoffers@gmail.com>
2016-10-17 11:09:55 -0200

    Adds icons and splash screen to mobile project

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+npm install
 npm run build -- keep
 
 cd desktop
---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -r mobile/www desktop/www desktop/build desktop/dist 
+rm -r bower_components node_modules
+
