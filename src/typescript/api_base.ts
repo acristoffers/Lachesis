a593d71a3999b0e3c4fcad83546c09acab70a43d
Álan Crístoffer <acristoffers@gmail.com>
2017-10-25 17:02:00 -0200

    Fixes missing imports

--- src/typescript/api_base.ts
+++ src/typescript/api_base.ts
@@ -23,6 +23,8 @@ THE SOFTWARE.
 import { SharedData } from './shared_data.service'
 import { Http, Response, Headers, RequestOptions } from '@angular/http'
 import { Observable } from 'rxjs'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/mergeMap'
 
 export class APIBase {
     protected constructor(
